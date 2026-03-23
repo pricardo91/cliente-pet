1cd0664ebea075d870407d8fa11b3c16a351d6a7
Ricardo Silva <pricardo.sribeiro@gmail.com>
2026-03-23 13:42:19 -0300

    feat: implementa fluxo para service do cliente

--- src/main/java/br/com/petz/clientepet/cliente/application/api/ClienteController.java
+++ src/main/java/br/com/petz/clientepet/cliente/application/api/ClienteController.java
@@ -1,15 +1,22 @@
 package br.com.petz.clientepet.cliente.application.api;
 
+import br.com.petz.clientepet.cliente.application.service.ClienteService;
+import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
 @RestController
+@RequiredArgsConstructor
 public class ClienteController implements ClienteAPI {
+
+    private final ClienteService clienteService;
 
     @Override
     public ClienteResponse postCliente(ClienteRequest clienteRequest) {
         log.info("[inicia] {} - postCliente", getClass().getSimpleName());
+        ClienteResponse clienteCriado = clienteService.criarCliente(clienteRequest);
         log.info("[finaliza] {} - postCliente", getClass().getSimpleName());
         return null;
     }
