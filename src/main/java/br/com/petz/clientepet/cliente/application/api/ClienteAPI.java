48dd2c14ee49f484f68dc1f178077c60fec75e9d
Ricardo Silva <pricardo.sribeiro@gmail.com>
2026-03-23 13:29:54 -0300

    feat: implementa controller do cliente

--- src/main/java/br/com/petz/clientepet/cliente/application/api/ClienteAPI.java
+++ src/main/java/br/com/petz/clientepet/cliente/application/api/ClienteAPI.java
@@ -4,7 +4,6 @@ import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
-@RestController
 @RequestMapping("/v1/cliente")
 public interface ClienteAPI {
 
--- src/main/java/br/com/petz/clientepet/cliente/application/api/ClienteController.java
+++ src/main/java/br/com/petz/clientepet/cliente/application/api/ClienteController.java
@@ -1,4 +1,16 @@
 package br.com.petz.clientepet.cliente.application.api;
 
-public class ClienteController {
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+public class ClienteController implements ClienteAPI {
+
+    @Override
+    public ClienteResponse postCliente(ClienteRequest clienteRequest) {
+        log.info("[inicia] {} - postCliente", getClass().getSimpleName());
+        log.info("[finaliza] {} - postCliente", getClass().getSimpleName());
+        return null;
+    }
 }
