8b5cf9ece642dbaae49a2dd217d9163e2090ebad
Ricardo Silva <pricardo.sribeiro@gmail.com>
2026-03-28 01:47:11 -0300

    feat: implementa metodo converte lista entidade cliente

--- src/main/java/br/com/petz/clientepet/cliente/application/api/ClienteListResponse.java
+++ src/main/java/br/com/petz/clientepet/cliente/application/api/ClienteListResponse.java
@@ -1,10 +1,12 @@
 package br.com.petz.clientepet.cliente.application.api;
 
 import br.com.petz.clientepet.cliente.domain.Cliente;
+import lombok.Value;
 
 import java.util.List;
 import java.util.UUID;
 
+@Value
 public class ClienteListResponse {
 
     private UUID idCliente;
@@ -13,7 +15,17 @@ public class ClienteListResponse {
     private String celular;
     private String cpf;
 
+    public ClienteListResponse(Cliente cliente) {
+        this.idCliente = cliente.getIdCliente();
+        this.nomeCompleto = cliente.getNomeCompleto();
+        this.email = cliente.getEmail();
+        this.celular = cliente.getCelular();
+        this.cpf = cliente.getCpf();
+    }
+
     public static List<ClienteListResponse> converte(List<Cliente> clientes) {
-        return null;
+        return clientes.stream()
+                .map(ClienteListResponse::new)
+                .toList();
     }
 }
--- src/main/java/br/com/petz/clientepet/cliente/application/repository/ClienteRepository.java
+++ src/main/java/br/com/petz/clientepet/cliente/application/repository/ClienteRepository.java
@@ -1,6 +1,5 @@
 package br.com.petz.clientepet.cliente.application.repository;
 
-import br.com.petz.clientepet.cliente.application.api.ClienteListResponse;
 import br.com.petz.clientepet.cliente.domain.Cliente;
 
 import java.util.List;
@@ -8,5 +7,6 @@ import java.util.List;
 public interface ClienteRepository {
 
     Cliente salva(Cliente cliente);
+
     List<Cliente> buscaTodosClientes();
 }
